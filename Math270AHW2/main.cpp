41bdadb5bae11147f5bd42bc99dcf85bfe4729f8
Tianyang Zhang <wsbbb331@gmail.com>
2016-11-12 12:06:38 -0800

    Initial Commit

---
+++ Math270AHW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Math270AHW2
+//
+//  Created by Tianyang Zhang on 11/12/16.
+//  Copyright © 2016 UCLA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
