8bc44021930ad525d9fa27ef6e6af775a2a98072
Tianyang Zhang <wsbbb331@gmail.com>
2016-11-12 13:53:49 -0800

    init

---
+++ Math270AHW2/Math270AHW2/main.cpp
@@ -0,0 +1,62 @@
+#include <cmath>
+#include "Tools.h"
+#include "ImplicitQRSVD.h"
+#include "SymmetricTridiagonal.h"
+#include "SimulationDriver.h"
+#include "EnergyTests.h"
+
+void EnergyTest(){
+  typedef double T;
+  typedef Eigen::Matrix<T,Eigen::Dynamic,1> TVect;
+  int N=5;
+  T a=(T)0,b=(T)1;
+  T dX=(b-a)/(T)(N-1);
+  JIXIE::NeoHookean<T> nh((T)1);
+  JIXIE::LinearElasticity<T> le((T)1);
+  JIXIE::FEMHyperelasticity<T> fem(a,dX,N,nh);
+  TVect x(N),f(N);
+  f=TVect::Zero(N);
+  for(int i=0;i<N;i++) x(i)=a+dX*(T)i;
+
+  JIXIE::EnergyTest<T> et("output",fem,10);
+  et.RefinementTest(x);
+
+}
+
+void ElasticitySimulation(){
+  typedef double T;
+  typedef Eigen::Matrix<T,Eigen::Dynamic,1> TVect;
+  int N=5;
+  T a=(T)0,b=(T)1;
+  T dX=(b-a)/(T)(N-1);
+  T dt=(T).01;
+  std::string output_dir("output");
+  JIXIE::ElasticityDriver<T> driver((T)1,30,dt,N,a,dX,output_dir);
+  driver.RunSimulation();
+}
+
+void ConvertBinaryToDat(){
+  typedef double T;
+  typedef Eigen::Matrix<T,Eigen::Dynamic,1> TVect;
+  TVect x,v; int N=0,frame=0;
+
+  std::string data_dir("output");
+  std::string output_dat_dir("output/matlab");
+
+  while(JIXIE::ElasticityDriver<T>::Read_State(x,v,N,data_dir,frame)){
+    char str[12];
+    sprintf(str, "%d", frame++);
+    std::string frame_name(str);
+    std::string positions_string(std::string("particle_x_")+frame_name);
+    std::string velocities_string(std::string("particle_v_")+frame_name);
+    FILE_IO::Write_DAT_File(std::string(output_dat_dir+std::string("/")+positions_string+std::string(".dat")),x);
+    FILE_IO::Write_DAT_File(std::string(output_dat_dir+std::string("/")+velocities_string+std::string(".dat")),x);
+  }
+}
+
+int main()
+{
+  //EnergyTest();
+  ElasticitySimulation();
+  //ConvertBinaryToDat();
+}
